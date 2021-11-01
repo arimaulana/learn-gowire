0b1c789e57384995e6c75466eef4bb5aec87d951
arimaulana <maulana.ari@protonmail.com>
2021-11-02 01:36:24 +0700

    Add wire generated code as injector

---
+++ README.md
@@ -0,0 +1,3 @@
+
+## Troubleshoot
+- https://github.com/google/wire/discussions/283 (wire redeclare the injector in generated code)
--- wire.go
+++ wire.go
@@ -1,8 +1,11 @@
+//go:build wireinject
+// +build wireinject
+
 package main
 
+import "github.com/google/wire"
+
 func InitializeEvent() Event {
-	message := NewMessage()
-	greeter := NewGreeter(message)
-	event := NewEvent(greeter)
-	return event
+	wire.Build(NewEvent, NewGreeter, NewMessage)
+	return Event{}
 }
